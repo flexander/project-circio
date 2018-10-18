f5bfb74e9403ad932fb0b43aa73f85bb992e3bbc
Alex Akran <akran91@gmail.com>
2018-10-18 14:27:33 +0100

    Adds a function for creating controls

--- scripts/controls.js
+++ scripts/controls.js
@@ -10,7 +10,8 @@ export default class Controls {
 
         this.engine = engine;
         this.painter = painter;
-        this.location = (typeof options.location !== 'undefined') ? options.location: document.body;
+        this.actionLocation = (typeof options.actionLocation !== 'undefined') ? options.actionLocation: document.body;
+        this.controlLocation = (typeof options.controlLocation !== 'undefined') ? options.controlLocation: document.body;
     }
 
     showActions () {
@@ -38,7 +39,7 @@ export default class Controls {
         actionContainer.append(showGuides);
         actionContainer.append(paused);
         actionContainer.append(clear);
-        this.location.prepend(actionContainer);
+        this.actionLocation.prepend(actionContainer);
 
         // Toggle show guides state
         showGuides.addEventListener('click', () => {
@@ -62,5 +63,57 @@ export default class Controls {
             this.painter.clear();
             return;
         });
+
+        return this;
+    }
+
+    showControls() {
+
+        let controlContainer = document.createElement('div');
+        controlContainer.classList.add('circle-controls');
+
+        this.engine.list.forEach(circle => {
+            let circleControls = document.createElement('div');
+            circleControls.classList.add('circle-control');
+            circleControls.setAttribute('data-circle-id', circle.id);
+
+            let circleSteps = this.createControl('steps','number',{
+                legend:'circle #'+circle.id,
+                value:circle.steps,
+            });
+            circleControls.append(circleSteps);
+
+            controlContainer.append(circleControls);
+        });
+
+        this.controlLocation.append(controlContainer);
+
+        return this;
+    }
+
+    createControl(name, type = 'text', options = {}) {
+        let container = document.createElement('fieldset');
+        let label = document.createElement('label');
+        let input = document.createElement('input');
+
+        container.classList.add('control', "control-"+name);
+        label.innerHTML = name;
+        input.type = type;
+        input.name = name;
+
+        if(typeof options.value !== 'undefined') {
+            input.value = options.value;
+        }
+
+        if(typeof options.legend !== 'undefined') {
+            let legend = document.createElement('legend');
+            legend.innerHTML = options.legend;
+            container.append(legend);
+        }
+
+        container.append(label);
+        container.append(input);
+
+        return container;
     }
 }
