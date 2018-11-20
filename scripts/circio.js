7b6b1e3b05126128cd91e36f8eab00dda1625ff1
Alex Akran <akran91@gmail.com>
2018-11-20 13:55:53 +0000

    Updates example

--- scripts/circio.js
+++ scripts/circio.js
@@ -3,5 +3,6 @@ import Painter from 'circio-painter';
 import Controls from 'circio-controls';
 import Circle from 'circio-shapes';
 import Zen from 'circio-zen';
+import Storage from './local_modules/circio-storage.js';
 
-export {Engine, Painter, Controls, Circle, Zen};
+export {Engine, Painter, Controls, Circle, Zen, Storage};
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
-import {Engine, Painter, Controls, Zen, Circle} from './circio.js';
+import {Engine, Painter, Controls, Zen, Circle, Storage} from './circio.js';
 
 const engine = window.engine = new Engine({
     width: 700,
     height: 700,
-    paused: true
+    paused: false
 });
 
 const painter = window.painter = new Painter(engine, {
@@ -22,9 +22,9 @@ const zen = window.zen = new Zen(engine);
 
 const A = new Circle({
     radius: 60,
-    direction: 'cw',
-    steps: 500,
-    radians: Math.PI/2
+    steps: 4,
+    radians: Math.PI/2,
+    clockwise: false,
 });
 
 const B = new Circle({
@@ -38,6 +38,8 @@ const C = new Circle({
     radius: 20,
     steps: 500,
     parent: B,
+    outside: true,
+    clockwise: false,
 
 });
 
@@ -50,7 +52,7 @@ const D = new Circle({
 
 engine.addCircles([A, B, C, D]).calculateCircles();
 
-painter.addCircleBrush(D, {color:'#719cf9'});
+painter.addCircleBrush(D, {color:'rgba(80,201,12,0.1)', link: true});
 
 controls.showActions().showControls();
 engine.addCallback(painter.drawCircles.bind(painter));
