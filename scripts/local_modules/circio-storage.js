fbbe5db83960ea4069b6d0330f9b97a9ad766268
Alex Akran <akran91@gmail.com>
2018-11-29 13:46:10 +0000

    Adds export method

--- scripts/local_modules/circio-storage.js
+++ scripts/local_modules/circio-storage.js
@@ -1,4 +1,28 @@
 export default class Storage {
-    constructor() {
+    constructor(engine = false, painter = false, options) {
+        this.engine = engine;
+        this.painter = painter;
+    }
+
+    export(encode = true) {
+        const data = {};
+
+        if(this.engine !== false) {
+            data.engineData = this.engine.export(false);
+        }
+
+        if(this.painter !== false) {
+            data.painterData = this.painter.export(false);
+        }
+
+        if(encode === true) {
+            return btoa(JSON.stringify(data));
+        }
+
+        return data;
+    }
+
+    store(data) {
+        
     }
 }
