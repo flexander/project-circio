f63d94abd2eb66e8569f8124bdee6e0d96645ffd
Alex Akran <akran91@gmail.com>
2019-07-24 14:18:15 +0100

    Adds babel and browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 'use strict';
 
 var gulp = require('gulp');
+var babel = require('gulp-babel');
+var browserify = require('gulp-browserify');
+
 
 gulp.task('js', function() {
     return gulp.src('scripts/*.js', { sourcemaps: true })
         .pipe(gulp.dest('public/js', { sourcemaps: true }));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src(
+        ['scripts/fourCircles.js'])
+        .pipe(babel({presets: ["@babel/preset-env"]}))
+        .pipe(browserify())
+        .pipe(gulp.dest('public/js'))
 });
