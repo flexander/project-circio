9b5c1375422887caeca417630d65322c0efc9a54
Alex Akran <akran91@gmail.com>
2019-08-15 10:31:31 +0100

    Adds support for creating multiple frames

--- scripts/node.js
+++ scripts/node.js
@@ -7,7 +7,8 @@ const fs = require('fs');
 const canvas = createCanvas(1080, 1080);
 const stream = canvas.createPNGStream();
 
-const data = {"engineData":{"engine":{"interval":2,"height":1080,"width":1080,"paused":false},"list":[{"id":0,"radius":400,"clockwise":false,"outside":false,"steps":10000,"fixed":true,"stepMod":0},{"id":1,"radius":200,"clockwise":true,"outside":false,"steps":500,"fixed":true,"stepMod":0},{"id":2,"radius":100,"clockwise":false,"outside":false,"steps":250,"fixed":true,"stepMod":0},{"id":3,"radius":50,"clockwise":true,"outside":false,"steps":125,"fixed":true,"stepMod":0}]},"painterData":{"painter":{"draw":true,"color":"#ffffff","point":0.5,"backgroundFill":"#202020"},"brushes":[null,null,null,[{"color":"#ffffff","point":1,"offset":-400,"degrees":0,"link":false}]]}};
+const engineData = {"engine":{"interval":2,"height":1080,"width":1080,"paused":false},"list":[{"id":0,"radius":400,"clockwise":false,"outside":false,"steps":10000,"fixed":true,"stepMod":0},{"id":1,"radius":200,"clockwise":true,"outside":false,"steps":500,"fixed":true,"stepMod":0},{"id":2,"radius":100,"clockwise":false,"outside":false,"steps":250,"fixed":true,"stepMod":0},{"id":3,"radius":50,"clockwise":true,"outside":false,"steps":125,"fixed":true,"stepMod":0}]};
+const painterData = {"painter":{"draw":true,"color":"#ffffff","point":0.5,"backgroundFill":"#202020"},"brushes":[null,null,null,[{"color":"#ffffff","point":1,"offset":-400,"degrees":0,"link":false}]]};
 
 const engine = new Engine({
     paused: false,
@@ -23,17 +24,23 @@ const painter = new Painter(engine, {
     canvas: canvas,
 });
 
-engine.import(data.engineData);
-painter.import(data.painterData);
+engine.import(engineData);
+painter.import(painterData);
 
 engine.addCallback(painter.drawCircles.bind(painter));
 
-const out = fs.createWriteStream(__dirname + '/../output/test.png');
-
 const steps = args['steps'] !== undefined ? args['steps']: 1000;
+const frames = args['frames'] !== undefined ? args['frames']: 1;
 
-for (let i = 0; i < steps; i++) {
-    engine.runOnce();
+for (let f = 0; f < frames; f++) {
+    painter.fillBackground();
+    let fileName = 'frame-'+ f.toString().padStart(frames.toString().length, '0') +'.png';
+    const out = fs.createWriteStream(__dirname + '/../output/'+fileName);
+    console.log(fileName);
+
+    for (let s = 0; s < steps; s++) {
+        engine.runOnce();
+    }
+
+    stream.pipe(out);
 }
-
-stream.pipe(out);
