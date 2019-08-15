469b386b3e8132c84850967adfb930f14977ba4d
Alex Akran <akran91@gmail.com>
2019-08-15 14:02:16 +0100

    Animates frames

--- scripts/node.js
+++ scripts/node.js
@@ -5,10 +5,9 @@ const args = require('minimist')(process.argv.slice(2));
 const fs = require('fs');
 
 const canvas = createCanvas(1080, 1080);
-const stream = canvas.createPNGStream();
 
 const engineData = {"engine":{"interval":2,"height":1080,"width":1080,"paused":false},"list":[{"id":0,"radius":400,"clockwise":false,"outside":false,"steps":10000,"fixed":true,"stepMod":0},{"id":1,"radius":200,"clockwise":true,"outside":false,"steps":500,"fixed":true,"stepMod":0},{"id":2,"radius":100,"clockwise":false,"outside":false,"steps":250,"fixed":true,"stepMod":0},{"id":3,"radius":50,"clockwise":true,"outside":false,"steps":125,"fixed":true,"stepMod":0}]};
-const painterData = {"painter":{"draw":true,"color":"#ffffff","point":0.5,"backgroundFill":"#202020"},"brushes":[null,null,null,[{"color":"#ffffff","point":1,"offset":-400,"degrees":0,"link":false}]]};
+const painterData = {"painter":{"draw":true,"color":"#ffffff","point":0.5,"backgroundFill":"#1b374c"},"brushes":[null,null,null,[{"color":"#ffffff","point":1,"offset":0,"degrees":0,"link":false}]]};
 
 const engine = new Engine({
     paused: false,
@@ -18,9 +17,6 @@ const engine = new Engine({
 });
 
 const painter = new Painter(engine, {
-    backgroundFill: "#1b374c",
-    showGuide: true,
-    color: '#ffffff',
     canvas: canvas,
 });
 
@@ -30,22 +26,30 @@ painter.import(painterData);
 engine.addCallback(painter.drawCircles.bind(painter));
 
 const steps = args['steps'] !== undefined ? args['steps']: 1000;
-const frames = args['frames'] !== undefined ? args['frames']: 1;
+const startFrame = args['start'] !== undefined ? args['start']: 1;
+const endFrame = args['end'] !== undefined ? args['end']: 1;
 const project = args['project'] !== undefined ? args['project']: Date.now();
+
 const dir = __dirname + '/../output/' + project;
 if (!fs.existsSync(dir)){
     fs.mkdirSync(dir);
 }
 
-for (let f = 0; f < frames; f++) {
+for (let f = startFrame; f <= endFrame; f++) {
+    console.log(f + ' of ' + endFrame);
     painter.fillBackground();
-    let fileName = '/frame-'+ f.toString().padStart((frames.toString().length-1) , '0') +'.png';
-    const out = fs.createWriteStream(__dirname + '/../output/' + project + fileName);
-    console.log(project + fileName);
+    engine.reset();
+    painter.brushes[3][0].degrees = f;
+    painter.brushes[3][0].offset = -800 + f;
+    let fileName = project + '/frame-'+ f.toString().padStart(10 , '0') +'.png';
+    draw(fileName, engine);
+}
 
+function draw (fileName, engine) {
     for (let s = 0; s < steps; s++) {
         engine.runOnce();
     }
 
-    stream.pipe(out);
+    const buffer = canvas.toBuffer();
+    fs.writeFileSync(__dirname + '/../output/' + fileName, buffer);
 }
