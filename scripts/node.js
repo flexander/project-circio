bd62254a65a6a1e3decf1d7bfffcdaff5478c77e
Alex A <akran91@gmail.com>
2019-08-18 18:25:20 +0100

    Adds 2 circle blueprint, ands bash script

--- scripts/node.js
+++ scripts/node.js
@@ -18,21 +18,19 @@ const painter = new Painter(engine, {
 });
 
 const engineData = {"engine":{"height":1080,"width":1080},
-    "list":[
-        {"id":0,"radius":5,"clockwise":true,"outside":false,"steps":0,"fixed":true, radians: (Math.PI * 1.5)},
-        {"id":1,"radius":100,"clockwise":true,"outside":true,"steps":40000,"fixed":true},
-        {"id":2,"radius":50,"clockwise":false,"outside":false,"steps":500,"fixed":true},
-        {"id":3,"radius":25,"clockwise":false,"outside":true,"steps":1000,"fixed":true},
-        {"id":4,"radius":25,"clockwise":false,"outside":false,"steps":1000,"fixed":true},
-    ]};
-const painterData = {"painter":{"backgroundFill":"#222222"},
-    "brushes":[
-        null,
-        null,
-        null,
-        null,
-        [{"color":"#e51c4620","point":2,"offset":0,"degrees":0,"link":false}]
-    ]};
+"list":[
+    {"id":0,"radius":300,"clockwise":false,"outside":false,"steps":10000,"fixed":true},
+    {"id":1,"radius":200,"clockwise":true,"outside":false,"steps":5000,"fixed":true},
+    {"id":2,"radius":50,"clockwise":false,"outside":false,"steps":250,"fixed":true},
+    {"id":3,"radius":15,"clockwise":true,"outside":false,"steps":125,"fixed":true}
+]};
+const painterData = {"painter":{"backgroundFill":"#ffffff"},
+"brushes":[
+    null,
+    null,
+    null,
+    [{"color":"#00000050","point":1,"offset":0,"degrees":0,"link":false}]
+]};
 
 engine.import(engineData);
 painter.import(painterData);
@@ -42,7 +40,7 @@ engine.addCallback(painter.drawCircles.bind(painter));
 const steps = args['steps'] !== undefined ? args['steps']: 1000;
 const startFrame = args['start'] !== undefined ? args['start']: 1;
 const endFrame = args['end'] !== undefined ? args['end']: 1;
-const project = args['project'] !== undefined ? args['project']: Date.now();
+const name = args['name'] !== undefined ? args['name']: Date.now();
 const offset = 180;
 
 const dir = __dirname + '/../output/' + project;
@@ -55,12 +53,12 @@ for (let f = startFrame; f <= endFrame; f++) {
     painter.fillBackground();
     engine.reset();
 
-    painter.brushes[4][0].degrees = f;
+    painter.brushes[3][0].degrees = f;
 
     if(f <= (2 * offset)) {
-        painter.brushes[4][0].offset = (-1 * offset) + f;
+        painter.brushes[3][0].offset = (-1 * offset) + f;
     } else {
-        painter.brushes[4][0].offset = offset - (f - (2 * offset));
+        painter.brushes[3][0].offset = offset - (f - (2 * offset));
     }
 
     let fileName = project + '/frame-'+ f.toString().padStart(10 , '0') +'.png';
