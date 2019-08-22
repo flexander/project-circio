8909f57a3505b93dc02d8894f14b17df6182f892
MoeBrowne <oliver.browne@uplyme.com>
2019-08-22 08:19:22 +0100

    Moves the canvas elements to the HTML doc

--- src/main.ts
+++ src/main.ts
@@ -3,46 +3,31 @@ import Painter from "./modules/painter";
 import GuidePainter from "./modules/guidePainter";
 import {BlueprintStore} from "./modules/storeBlueprint";
 
+const canvasArea = <HTMLElement>document.querySelector('#circio .painter');
+const backgroundCanvasElement = <HTMLCanvasElement>canvasArea.querySelector('#background-canvas');
+const mainCanvasElement = <HTMLCanvasElement>canvasArea.querySelector('#main-canvas');
+const guideCanvasElement = <HTMLCanvasElement>canvasArea.querySelector('#guide-canvas');
 const storage = new BlueprintStore();
-
 const circ = storage.get('threeCircles');
 
-const canvasArea = document.querySelector('#circio .painter');
 canvasArea.style.transformOrigin = '0 0'; //scale from top left
 canvasArea.style.transform = 'scale(' + window.innerHeight / circ.height + ')';
 canvasArea.style.width = circ.width + 'px';
 canvasArea.style.height = circ.height + 'px';
 
-const background = document.createElement('canvas');
-background.setAttribute('id', 'background-canvas');
-canvasArea.appendChild(background);
-
-const canvas = document.createElement('canvas');
-canvas.setAttribute('id', 'main-canvas');
-canvasArea.appendChild(canvas);
-const canvasContext = canvas.getContext("2d");
-
-const guide = document.createElement('canvas');
-guide.setAttribute('id', 'guide-canvas');
-canvasArea.appendChild(guide);
-const guideContext = guide.getContext("2d");
-
 canvasArea.querySelectorAll('canvas').forEach(c => {
     c.setAttribute('height', canvasArea.style.height);
     c.setAttribute('width', canvasArea.style.width);
-    c.style.position = 'absolute';
 });
 
 
-const backgroundContext = background.getContext("2d");
-backgroundContext.beginPath();
-backgroundContext.rect(0, 0, circ.width, circ.height);
-backgroundContext.fillStyle = circ.backgroundFill;
-backgroundContext.fill();
+const backgroundCanvasContext = backgroundCanvasElement.getContext("2d");
+backgroundCanvasContext.fillStyle = circ.backgroundFill;
+backgroundCanvasContext.fillRect(0, 0, circ.width, circ.height);
 
 const engine = new Engine();
-const painter = new Painter(canvasContext);
-const guidePainter = new GuidePainter(guideContext);
+const painter = new Painter(mainCanvasElement.getContext("2d"));
+const guidePainter = new GuidePainter(guideCanvasElement.getContext("2d"));
 
 engine.import(circ);
 engine.addCallback(circ => painter.draw(circ));
