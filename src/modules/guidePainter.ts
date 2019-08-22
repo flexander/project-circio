4930c201f4c368090280d94efa99c6936e468ecb
MoeBrowne <oliver.browne@uplyme.com>
2019-08-22 07:58:35 +0100

    Fixes brush guide offset lines

--- src/modules/guidePainter.ts
+++ src/modules/guidePainter.ts
@@ -1,4 +1,4 @@
-import {CircInterface, CircleInterface, GuidePainterInterface} from "../structure";
+import {BrushInterface, CircInterface, CircleInterface, GuidePainterInterface} from "../structure";
 
 export default class GuidePainter implements GuidePainterInterface {
     protected canvasContext: CanvasRenderingContext2D;
@@ -45,10 +45,11 @@ export default class GuidePainter implements GuidePainterInterface {
         this.canvasContext.arc(circle.state.centre.x,circle.state.centre.y,circle.radius,0,2*Math.PI);
         this.canvasContext.stroke();
 
-        this.drawCircleRotation(circle);
+        this.drawRotationIndicator(circle);
+        circle.brushes.forEach((brush: BrushInterface) => this.drawBrushPoint(circle, brush));
     };
 
-    drawCircleRotation (circle: CircleInterface) {
+    drawRotationIndicator (circle: CircleInterface) {
         const color = '#FFF';
 
         this.canvasContext.fillStyle = color;
@@ -57,5 +58,19 @@ export default class GuidePainter implements GuidePainterInterface {
         this.canvasContext.arc(circle.state.drawPoint.x, circle.state.drawPoint.y, 4, 0, 2*Math.PI);
         this.canvasContext.fill();
     };
+
+    protected drawBrushPoint(circle: CircleInterface, brush: BrushInterface): void {
+        const color = '#FFF';
+        const brushPointX = circle.state.drawPoint.x + (Math.cos(circle.state.getAngle() + (brush.degrees * (Math.PI/180))) * brush.offset);
+        const brushPointY = circle.state.drawPoint.y + (Math.sin(circle.state.getAngle() + (brush.degrees * (Math.PI/180))) * brush.offset);
+
+        this.canvasContext.strokeStyle = color;
+        this.canvasContext.beginPath();
+        this.canvasContext.moveTo(circle.state.drawPoint.x, circle.state.drawPoint.y);
+        this.canvasContext.lineTo(brushPointX, brushPointY);
+        this.canvasContext.stroke();
+        this.canvasContext.arc(brushPointX, brushPointY, 4, 0, 2*Math.PI);
+        this.canvasContext.fill();
+    }
 
 }
