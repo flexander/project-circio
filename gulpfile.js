944d946503397fa6730437a836f04a542e7dece5
MoeBrowne <oliver.browne@uplyme.com>
2019-09-02 22:17:01 +0100

    Updates the build tools to build for both CLI and browser

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,10 @@ var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var tsify = require('tsify');
 const sass = require('gulp-sass');
+const ts = require('gulp-typescript');
+const tsProject = ts.createProject('./tsconfig.json');
 
-gulp.task('build:ts', function () {
+gulp.task('build:ts-browser', function () {
     return browserify()
         .add('./src/main.ts')
         .plugin(tsify)
@@ -16,6 +18,13 @@ gulp.task('build:ts', function () {
         .pipe(gulp.dest('./public/js'));
 });
 
+gulp.task('build:ts-cli', function () {
+    return tsProject.src()
+        .on('error', function (error) { console.error(error.toString()); })
+        .pipe(tsProject())
+        .js.pipe(gulp.dest('node'));
+});
+
 gulp.task('build:scss', () => {
     return gulp.src('./styles/*.scss')
         .pipe(sass())
@@ -24,6 +33,6 @@ gulp.task('build:scss', () => {
 });
 
 gulp.task('default', function() {
-    gulp.watch(['src/*.ts', 'src/**/*.ts'], gulp.series('build:ts'));
+    gulp.watch(['src/*.ts', 'src/**/*.ts'], gulp.parallel('build:ts-browser', 'build:ts-cli'));
     gulp.watch(['styles/*.scss'], gulp.series('build:scss'));
 });
