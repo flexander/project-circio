27508606a37828f33395d5cf5012180c51a4e3cc
MoeBrowne <oliver.browne@uplyme.com>
2019-09-02 14:22:37 +0100

    Adds missing SCSS compilation. Fixes #32

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var buffer = require('vinyl-buffer');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var tsify = require('tsify');
+const sass = require('gulp-sass');
 
 gulp.task('build:ts', function () {
     return browserify()
@@ -15,6 +16,14 @@ gulp.task('build:ts', function () {
         .pipe(gulp.dest('./public/js'));
 });
 
+gulp.task('build:scss', () => {
+    return gulp.src('./styles/*.scss')
+        .pipe(sass())
+        .on('error', function (error) { console.error(error.toString()); })
+        .pipe(gulp.dest('./public/css/'));
+});
+
 gulp.task('default', function() {
     gulp.watch(['src/*.ts', 'src/**/*.ts'], gulp.series('build:ts'));
+    gulp.watch(['styles/*.scss'], gulp.series('build:scss'));
 });
