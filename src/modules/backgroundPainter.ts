40eb6cbb62438b73a6b1250d73ea3d00854d6db7
MoeBrowne <oliver.browne@uplyme.com>
2019-09-03 13:20:21 +0100

    Updates the background painter to use the center as the origin

--- src/modules/backgroundPainter.ts
+++ src/modules/backgroundPainter.ts
@@ -1,22 +1,43 @@
-import {BackgroundPainterInterface, CircInterface} from "../structure";
+import {BackgroundPainterInterface, CircInterface, PositionInterface} from "../structure";
 
 export default class BackgroundPainter implements BackgroundPainterInterface {
     protected canvasContext: CanvasRenderingContext2D;
+    protected canvasCenter: PositionInterface = new CanvasCenter();
     
     constructor(canvasContext: CanvasRenderingContext2D) {
         this.canvasContext = canvasContext;
     }
 
     public draw(circ: CircInterface): void {
+        this.centerCanvas(circ);
+
         this.canvasContext.fillStyle = circ.backgroundFill;
-        this.canvasContext.fillRect(0, 0, circ.width, circ.height);
+        this.canvasContext.fillRect(-this.canvasContext.canvas.width/2, -this.canvasContext.canvas.height/2, this.canvasContext.canvas.width, this.canvasContext.canvas.height);
+    }
+
+    protected centerCanvas(circ: CircInterface) {
+        if (this.canvasCenter.x !== (circ.width/2)) {
+            this.canvasContext.translate(-this.canvasCenter.x, 0);
+            this.canvasContext.translate((circ.width/2), 0);
+            this.canvasCenter.x = (circ.width/2);
+        }
+        if (this.canvasCenter.y !== (circ.height/2)) {
+            this.canvasContext.translate(0,-this.canvasCenter.y);
+            this.canvasContext.translate(0,(circ.height/2));
+            this.canvasCenter.y = (circ.height/2);
+        }
     }
 
     clear(): void {
-        this.canvasContext.clearRect(0, 0, this.canvasContext.canvas.width, this.canvasContext.canvas.height);
+        this.canvasContext.clearRect(-this.canvasContext.canvas.width/2, -this.canvasContext.canvas.height/2, this.canvasContext.canvas.width, this.canvasContext.canvas.height);
     }
 
     exportImageAsDataURL(): string {
         return "";
     }
+}
+
+class CanvasCenter implements PositionInterface {
+    x: number = 0;
+    y: number = 0;
 }
