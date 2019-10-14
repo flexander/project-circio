ad92cd036172d87f1743c18ed1b8fc565fc3e988
Ashley Shea <ash@uk.ibm.com>
2019-10-14 11:29:57 +0200

    AddShape tests

--- src/modules/circ.test.ts
+++ src/modules/circ.test.ts
@@ -1,12 +1,43 @@
 import { Circ } from './Circ';
+import { Circle } from './Circle';
+import { ShapeAddEvent } from './events';
 
 describe('Circ', () => {
-    let testCirc;
+    let circ;
+    let shape1;
     beforeEach(() => {
-        testCirc = new Circ();
+        circ = new Circ();
+        circ.dispatchEvent = jest.fn();
+        shape1 = new Circle();
     });
 
     it('should create a valid circ', () => {
-        expect(testCirc).not.toBe(null);
+        expect(circ).not.toBe(null);
+    });
+
+    describe('addShape', () => {
+        beforeEach(() => {
+            circ.addShape(shape1);
+        });
+
+        it('should set the isRoot property of the first shape to true', () => {
+            expect(shape1.isRoot).toEqual(true);
+        });
+
+        it('should set the isRoot property of the second shape to false', () => {
+            const shape2 = new Circle();
+            circ.addShape(shape2);
+            expect(shape2.isRoot).toEqual(false);
+        });
+
+        it('should add the shape to the shapes array', () => {
+            const expectedShapes = [shape1];
+            expect(circ.getShapes()).toEqual(expectedShapes);
+        });
+
+        it('should call the dispatchEvent exactly once with the shape', () => {
+            expect(circ.dispatchEvent).toHaveBeenCalledTimes(1);
+            expect(circ.dispatchEvent).toHaveBeenCalledWith(new ShapeAddEvent(shape1));
+        });
     });
 });
